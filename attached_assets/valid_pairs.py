04966893375473295b28a81f56c982f85fd75a4c
bkdaku208 <bkdaku208@gmail.com>
2025-04-24 19:29:05 +0530

    Add files via upload

---
+++ attached_assets/valid_pairs.py
@@ -0,0 +1,12 @@
+# List of valid OTC pairs available on Quotex
+VALID_OTC_PAIRS = [
+    "USD/BRL (OTC)", "NZD/CAD (OTC)", "USD/EGP (OTC)", "USD/INR (OTC)",
+    "USD/BDT (OTC)", "USD/MXN (OTC)", "USD/PKR (OTC)", "CAD/CHF (OTC)",
+    "EUR/GBP", "EUR/SGD (OTC)", "USD/ARS (OTC)", "AUD/NZD (OTC)",
+    "GBP/CHF (OTC)", "USD/NGN (OTC)", "NZD/JPY (OTC)", "USD/IDR (OTC)",
+    "EUR/NZD (OTC)", "EUR/JPY", "USD/DZD (OTC)", "GBP/JPY", "NZD/CHF (OTC)",
+    "USD/COP (OTC)", "USD/PHP (OTC)", "CAD/JPY", "USD/TRY (OTC)",
+    "USD/ZAR (OTC)", "EUR/USD", "AUD/CHF", "CHF/JPY", "EUR/AUD", "EUR/CAD",
+    "EUR/CHF", "USD/JPY", "GBP/USD", "AUD/JPY", "GBP/CAD", "USD/CAD",
+    "NZD/USD (OTC)", "AUD/CAD", "AUD/USD", "GBP/AUD", "GBP/NZD (OTC)", "USD/CHF"
+]
